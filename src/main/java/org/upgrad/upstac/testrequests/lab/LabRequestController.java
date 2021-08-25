610751d17c07763f9212d9611be6d91f3fbca52a
GKSethi <gksethi@adobe.com>
2021-08-25 22:07:51 +0530

    Week 3 completed

--- src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
+++ src/main/java/org/upgrad/upstac/testrequests/lab/LabRequestController.java
@@ -64,16 +64,23 @@ public class LabRequestController {
     @GetMapping
     @PreAuthorize("hasAnyRole('TESTER')")
     public List<TestRequest> getForTester()  {
+    	try {
 
         // Implement This Method
 
         // Create an object of User class and store the current logged in user first
+    	  User tester = userLoggedInService.getLoggedInUser();
+ 
         //Implement this method to return the list of test requests assigned to current tester(make use of the above created User object)
         //Make use of the findByTester() method from testRequestQueryService class
         // For reference check the method getForTests() method from LabRequestController class
+    	  return testRequestQueryService.findByTester(tester);
 
-        throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED,"Not implemented"); // replace this line with your code
-
+    } catch (ConstraintViolationException e) {
+        throw asConstraintViolation(e);
+    }catch (AppException e) {
+        throw asBadRequest(e.getMessage());
+    }
 
     }
 
